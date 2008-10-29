cde6cb0008a64efe3245a83630aea05785806d1e
John F. Schank III <jschank@mac.com>
2008-10-29 01:38:21 -0400

    Added rake tasks for install and uninstall, fixed problems with both

--- install.rb
+++ install.rb
@@ -1,10 +1,14 @@
 # copy the version.yml.erb to some user editable location for example, in lib
 
-targetDir = File.join(RAILS_ROOT, 'lib/templates')
-sourceFile = File.join(File.dirname(__FILE__), 'lib/templates/version.yml.erb')
+targetTemplateDir = File.join(RAILS_ROOT, 'lib/templates')
+sourceTemplateFile = File.join(File.dirname(__FILE__), 'lib/templates/version.yml.erb')
 
-FileUtils.mkdir( targetDir, :verbose => true)
-FileUtils.cp( sourceFile, targetDir,:verbose => true)
+sourceSampleFile = File.join(File.dirname(__FILE__), 'lib/templates/version.yml')
+targetSampleDir = File.join(RAILS_ROOT, '/config')
+
+FileUtils.mkdir( targetTemplateDir, :verbose => true) unless File.exists?(targetTemplateDir)
+FileUtils.cp( sourceTemplateFile, targetTemplateDir,:verbose => true)
+FileUtils.cp( sourceSampleFile, targetSampleDir, :verbose => true )
 
 # Show the README text file
 # puts IO.read(File.join(File.dirname(__FILE__), 'README'))
--- tasks/app_version_tasks.rake
+++ tasks/app_version_tasks.rake
@@ -4,4 +4,14 @@ namespace :app do
     require File.join(File.dirname(__FILE__), "../lib/app_version.rb")
     puts "Application version: " << Version.load("#{RAILS_ROOT}/config/version.yml").to_s
   end
+
+  desc 'Configure for initial install.'
+  task :install do
+    require File.join(File.dirname(__FILE__), "../install.rb")
+  end
+
+  desc 'Clean up prior to removal.'
+  task :uninstall do
+    require File.join(File.dirname(__FILE__), "../uninstall.rb")
+  end
 end
