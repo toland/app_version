23f67bee892586db1a92c441bbc6d56a58eabc0d
Phil Toland <phil.toland@gmail.com>
2010-04-19 13:53:07 -0500

    Quietly bail if no version template file exists

--- recipes/app_version.rb
+++ recipes/app_version.rb
@@ -1,14 +1,16 @@
 # this is a capistrano recipe that will use the templated version.yml.erb
-# to produce the actual version.yml that the app_version plugin uses to 
+# to produce the actual version.yml that the app_version plugin uses to
 # store version information.
 
 namespace :app_version do
 
   desc "Generate version.yml from variables"
   task :generate_version_info, :roles => :app do
-    template = File.read("lib/templates/version.yml.erb")
-    result   = ERB.new(template).result(binding)
-    put result, "#{release_path}/config/version.yml"
+    if File.exist?("lib/templates/version.yml.erb")
+      template = File.read("lib/templates/version.yml.erb")
+      result   = ERB.new(template).result(binding)
+      put result, "#{release_path}/config/version.yml"
+    end
   end
   after "deploy:update_code", "app_version:generate_version_info"
 
