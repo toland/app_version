780eb94cd6f802bf3b9a2c657e735939356792a4
dreinavarro <dreinavarro@gmail.com>
2011-04-17 17:49:49 +0800

    RAILS ROOT reference

--- install.rb
+++ install.rb
@@ -1,10 +1,10 @@
 # copy the version.yml.erb to some user editable location for example, in lib
 
-targetTemplateDir = File.join(RAILS_ROOT, 'lib/templates')
+targetTemplateDir = File.join(Rails.root.to_s, 'lib/templates')
 sourceTemplateFile = File.join(File.dirname(__FILE__), 'lib/templates/version.yml.erb')
 
 sourceSampleFile = File.join(File.dirname(__FILE__), 'lib/templates/version.yml')
-targetSampleDir = File.join(RAILS_ROOT, '/config')
+targetSampleDir = File.join(Rails.root.to_s, '/config')
 
 FileUtils.mkdir( targetTemplateDir, :verbose => true) unless File.exists?(targetTemplateDir)
 FileUtils.cp( sourceTemplateFile, targetTemplateDir,:verbose => true)
